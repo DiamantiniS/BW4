eff04c1e007702831d0e172c0537dc270f898547
ziobaga <adrianoleanza01@gmail.com>
2024-07-08 10:54:07 +0200

    Classi delle tab

---
+++ BW4/Models/Prodotto.cs
@@ -0,0 +1,16 @@
+﻿namespace BW4.Models
+{
+    public class Prodotto
+    {
+        public int ProdottoID { get; set; }
+        public string NomeProdotto { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Prezzo { get; set; }
+        public int QuantitaDisponibile { get; set; }
+        public int CategoriaID { get; set; }
+        public string ImmagineCoverHome { get; set; }
+        public string ImmagineDettagli { get; set; }
+        public Categoria Categoria { get; set; }
+        public DateTime DataAggiunta { get; set; }
+    }
+}
